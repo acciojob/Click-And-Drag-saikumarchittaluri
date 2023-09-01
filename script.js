b8258bf4079ddd31ec50ec86016ba94e8db743d3
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-09-01 22:42:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // Your code here.
+ const items = document.querySelector('.items');
+    let isDragging = false;
+    let startX;
+    let scrollLeft;
+
+    items.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      startX = e.pageX - items.offsetLeft;
+      scrollLeft = items.scrollLeft;
+      items.classList.add('active');
+    });
+
+    items.addEventListener('mousemove', (e) => {
+      if (!isDragging) return;
+      e.preventDefault();
+
+      const x = e.pageX - items.offsetLeft;
+      const walk = (x - startX) * 3;
+      items.scrollLeft = scrollLeft - walk;
+    });
+
+    items.addEventListener('mouseup', () => {
+      isDragging = false;
+      items.classList.remove('active');
+    });
+
+    items.addEventListener('mouseleave', () => {
+      isDragging = false;
+      items.classList.remove('active');
+    });
